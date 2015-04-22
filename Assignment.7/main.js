b666b8d0a671a0c48aab2a91bec2ee1d0a540057
rickyaward <rickyaward@gmail.com>
2015-04-22 14:46:43 -0400

    how is it looking?

---
+++ Assignment.7/main.js
@@ -0,0 +1,11 @@
+var name = "hello ricky";
+alert(name);
+var myArray = ["ford", "chevy", "dodge", "toyota"];
+console.log(myArray[2]);
+
+var car = {
+	doors: 4,
+	honk: function(){
+		console.log("honk");
+	}
+};
